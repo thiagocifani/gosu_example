86dcefeb180f5291099667215c12cc228bfe2cc7
Thiago Cifani <cifani.thiago@gmail.com>
2015-07-22 15:01:42 -0300

    Changes player behavior.

--- player.rb
+++ player.rb
@@ -1,28 +1,64 @@
 class Player
-  def initialize
-    @animation = [Gosu::Image.new("media/ryu/idle-1.gif"),
-                  Gosu::Image.new("media/ryu/idle-2.gif"),
-                  Gosu::Image.new("media/ryu/idle-3.gif")]
+  attr_reader :lives, :x, :y, :laser
 
-    @image = Gosu::Image.new("media/ryu/idle-1.gif")
-    @x = 400.0
-    @y = 150.0
-  end
-
-  def warp(x, y)
-    @x, @y = x, y
-  end
-
-  def left
-    @x -= 1.0
-  end
-
-  def right
-    @x += 1.0
+  def initialize(window)
+    @window    = window
+    @image     = Gosu::Image.new(@window, "media/player.png", true)
+    @x         = 0
+    @y         = window.height - 80
+    @explosion = Gosu::Image.new(@window, "media/explosion.png", true)
+    @exploded  = false
+    @lives     = 3
+    @laser     = Laser.new(self, @window)
   end
 
   def draw
-    img = @animation[Gosu::milliseconds / 100 % @animation.size];
-    img.draw(@x, @y,0, 3, 3)
+    if @exploded
+      @explosion.draw(@x, @y, 4)
+    else
+      @image.draw(@x, @y, 1)
+      @laser.draw
+    end
+  end
+
+  def update
+    if @window.button_down? Gosu::Button::KbLeft
+      move_left
+    end
+    if @window.button_down? Gosu::Button::KbRight
+      move_right
+    end
+    if @window.button_down? Gosu::Button::KbSpace
+      @laser.shoot
+    end
+    @laser.update
+  end
+
+  def move_left
+    @x -= 3
+    if @x < 0
+      @x = 0
+    end
+  end
+
+  def move_right
+    @x += 3
+    if @x > @window.width-50
+      @x = @window.width-50
+    end
+  end
+
+  def hit_by?(bullets)
+    @exploded = bullets.any? { |bullet| Gosu::distance(bullet.x, bullet.y, @x, @y) < 20 }
+
+    if @exploded
+      @lives -= 1
+    end
+
+    @exploded
+  end
+
+  def reset_position
+    @x = rand(@window.width)
   end
 end
--- tutorial.rb
+++ tutorial.rb
@@ -1,32 +1,66 @@
 require 'gosu'
-require_relative "./backdrop"
+require_relative "./bullet"
 require_relative "./player"
+require_relative "./laser"
+require_relative "./timer"
 
 class GameWindow < Gosu::Window
+  attr_accessor :score
   def initialize
-    super 640, 480
-    self.caption = "Gosu Tutorial Game"
+    super(600, 400, false)
 
-    @backdrop = Backdrop.new(self, "background.jpg")
-    @player   = Player.new
-    @beep     = Gosu::Sample.new("media/teste.mp3")
-    @font     = Gosu::Font.new(20)
+    @background    = Gosu::Image.new(self, "media/starry-night.png", true)
+    @player        = Player.new(self)
+    @bullets       = 10.times.map { Bullet.new(self) }
+    @running       = true
+    @font          = Gosu::Font.new(self, Gosu::default_font_name, 20)
+    @game_over     = Gosu::Font.new(self, Gosu::default_font_name, 20)
+    @score_font   = Gosu::Font.new(self, Gosu::default_font_name, 20)
+    @timer         = Timer.new(self, @bullets)
+    @score        = 0
+    @game_over_now = false
   end
 
   def update
-    if Gosu::button_down? Gosu::KbLeft or Gosu::button_down? Gosu::GpLeft then
-      @player.left
+    if @running
+      if @player.hit_by? live_bullets
+        @running = false
+      else
+        run_game
+      end
     end
-    if Gosu::button_down? Gosu::KbRight or Gosu::button_down? Gosu::GpRight then
-      @player.right
+
+    if @running == false and button_down? Gosu::Button::KbR and @player.lives > 0
+      @running = true
+      @player.reset_position
+    end
+
+    if @player.lives == 0
+      @game_over_now = true
     end
   end
 
+  def run_game
+    live_bullets.each { |bullet| bullet.update(@player.laser) }
+    @player.update
+    @timer.update
+  end
+
+  def live_bullets
+    @bullets.select { |bullet| bullet.alive == true }
+  end
+
   def draw
-    @backdrop.draw
+    @background.draw(0,0,1)
     @player.draw
-    @beep.play
-    @font.draw("STREET FIGHTER", 10, 10, 3, 1.0, 1.0, 0xff_ffff00)
+    live_bullets.each { |bullet| bullet.draw}
+    @font.draw("Lives: #{@player.lives}", 10, 10, 3.0, 1.0, 1.0, 0xffffffff)
+    @score_font.draw("Score: #{@score}",  100, 10, 3.0, 1.0, 1.0, 0xffffffff)
+
+    if @game_over_now
+      @game_over.draw("GAME OVER", (self.width/2)-50, self.height/2, 3.0, 1.0, 1.0, 0xffffffff)
+    end
+
   end
 end
 
